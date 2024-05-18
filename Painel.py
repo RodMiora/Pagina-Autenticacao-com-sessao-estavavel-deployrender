4cd5a3bc85bb9e6045b399aedb5e67429835e82a
RodMiora <rodrigomiorin@gmail.com>
2024-05-17 22:57:35 -0300

    restaurando original

--- Painel.py
+++ Painel.py
@@ -7,45 +7,20 @@ from supabase import create_client, Client
 from config import SUBABASE_URL, SUBABASE_API_KEY
 from streamlit_option_menu import option_menu 
 
+
 # Importando Banco de Dados do SupaBase através de uma variável de ambiente permanente
 supabase: Client = create_client(SUBABASE_URL, SUBABASE_API_KEY)
+
 
 # Função para configurar o título da página
 def set_page_title(title):
     st.session_state['Bem Vindo'] = title
     st.markdown(f'<h1 style="font-weight:bold;">{title}</h1>', unsafe_allow_html=True)
 
+
 # Configurando a página
 st.set_page_config(page_title="Sua Sala", page_icon=":bar_chart:", layout="wide")
 
-# Definindo o conteúdo da página inicial
-def pagina_inicial():
-    st.title("Painel de Vendas by Rodrigo")
-    st.write("Bem-vindo à sua página inicial!")
-    # Adicione cores de fundo ou estilos adicionais, se desejar
-    st.markdown(
-        """
-        <style>
-        .stButton>button {
-            background-color: #007bff;
-            color: #ffffff;
-            font-size: 16px;
-            padding: 10px 20px;
-            border-radius: 5px;
-            cursor: pointer;
-        }
-        </style>
-        """
-    )
-    # Adicione um botão para redirecionar para a página de login
-    if st.button("Ir para a página de login"):
-        redirecionar_para_login()
-
-# Função para redirecionar para a página de login
-def redirecionar_para_login():
-    # Redireciona para a página de login
-    # Aqui você pode usar o streamlit's st.experimental_set_query_params() para passar quaisquer parâmetros necessários
-    st.experimental_rerun()
 
 # Carregando o arquivo de configuração YAML
 try:
@@ -55,11 +30,13 @@ except FileNotFoundError:
     print("O arquivo YAML não foi encontrado.")
     config = None
 
+
 # Verificando se o arquivo de configuração foi carregado corretamente
 if config is not None:
     credentials = config['credentials']
 else:
     print("Falha ao carregar o arquivo YAML.")
+
 
 # Inicializando o objeto de autenticação com a classe correta
 authenticator = stauth.Authenticate(
@@ -92,42 +69,37 @@ if authentication_status:
     if selected == "Painel":
         st.title(f"Você Selecionou {selected}")
 
-        # Página do Dashboard
-        if selected == 'Painel':
-            # Carregar dados para o dashboard
-            uploaded_file = st.file_uploader("Carregar banco de dados", type=["csv", "xlsx"])
-            if uploaded_file is not None:
-                df = pd.read_csv(uploaded_file)
-                # Exibindo todos os gráficos
-                col1, col2 = st.columns(2)
-                with col1:
-                    st.subheader('Gráfico de Linhas')
-                    fig_line = px.line(df)
-                    st.plotly_chart(fig_line)
-                with col2:
-                    st.subheader('Gráfico de Barras')
-                    fig_bar = px.bar(df)
-                    st.plotly_chart(fig_bar)
-                col3, col4 = st.columns(2)
-                with col3:
-                    st.subheader('Histograma')
-                    fig_hist = px.histogram(df)
-                    st.plotly_chart(fig_hist)
-                with col4:
-                    st.subheader('Gráfico de Pontos')
-                    fig_scatter = px.scatter(df)
-                    st.plotly_chart(fig_scatter)
+    
 
+    # Página do Dashboard
+    if selected == 'Painel':
+        # Carregar dados para o dashboard
+        uploaded_file = st.file_uploader("Carregar banco de dados", type=["csv", "xlsx"])
+        if uploaded_file is not None:
+            df = pd.read_csv(uploaded_file)
+            # Exibindo todos os gráficos
+            col1, col2 = st.columns(2)
+            with col1:
+                st.subheader('Gráfico de Linhas')
+                fig_line = px.line(df)
+                st.plotly_chart(fig_line)
+            with col2:
+                st.subheader('Gráfico de Barras')
+                fig_bar = px.bar(df)
+                st.plotly_chart(fig_bar)
+            col3, col4 = st.columns(2)
+            with col3:
+                st.subheader('Histograma')
+                fig_hist = px.histogram(df)
+                st.plotly_chart(fig_hist)
+            with col4:
+                st.subheader('Gráfico de Pontos')
+                fig_scatter = px.scatter(df)
+                st.plotly_chart(fig_scatter)
+    
+   
 elif authentication_status == False:
     st.error('Nome de usuário/senha incorreto')
 elif authentication_status == None:
     st.warning('Por favor, insira seu nome de usuário e senha')
 
-# Verifica se estamos na página inicial
-if st.session_state.get("pagina_inicial", True):
-    pagina_inicial()
-    st.session_state["pagina_inicial"] = False
-else:
-    # Se não estamos na página inicial, exibimos o conteúdo do Painel normalmente
-    set_page_title("Painel de Vendas by Rodrigo")
-    st.write("Este é o conteúdo do seu Painel de Vendas.")
